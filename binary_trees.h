7d20e7a749e323f2e6119073b80b21c305db2388
Ozioma7 <okonisaozioma@gmail.com>
2023-11-01 08:15:26 -0700

    corrected header file again

--- binary_trees.h
+++ binary_trees.h
@@ -7,13 +7,6 @@
 #include <string.h>
 #include <limits.h>
 
-unsigned char is_leaf(const binary_tree_t *node);
-size_t depth(const binary_tree_t *tree);
-const binary_tree_t *get_leaf(const binary_tree_t *tree);
-int is_perfect_recursive(const binary_tree_t *tree,
-		size_t leaf_depth, size_t level);
-int binary_tree_is_perfect(const binary_tree_t *tree);
-
 /**
  * struct binary_tree_s - Binary tree node
  *
